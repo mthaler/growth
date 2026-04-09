170d4e89bbf380597f38f2826b40e977fd7b5a85
Michael Thaler <michael.thaler2@gmail.com>
2026-04-09 12:11:05 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "gonum.org/v1/plot/plotter"
 
+func main() {
+	points := plotter.XYs{}
+	for i := 0; i <= 500; i++ {
+		t := float64(i) / 100.0
+		points = append(points, plotter.XY{
+			X: t,
+			Y: exponential(1, 2, 1, t),
+		})
+	}
+	b := bounds{
+		xmin: 0,
+		xmax: 5,
+		ymin: -0.5,
+		ymax: 0.5,
+	}
 }
