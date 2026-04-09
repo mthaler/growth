6eb287607d7dbb5f6d921b67bccdd7465bb86f20
Michael Thaler <michael.thaler2@gmail.com>
2026-04-09 15:40:10 +0200

    Updated logistic.png and main.go.

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"gonum.org/v1/plot/plotter"
 )
 
@@ -29,19 +27,10 @@ func main() {
 	var points2 plotter.XYs
 	for i := 0; i <= 4000; i++ {
 		t := float64(i) / 100.0
-		deltat := 1.0 / 100.0
-		if t == 0 {
-			points2 = append(points2, plotter.XY{
-				X: t,
-				Y: 0,
-			})
-		} else {
-			points2 = append(points2, plotter.XY{
-				X: t,
-				Y: logistic(points2[i-1].Y, 1, 1, deltat),
-			})
-		}
+		points2 = append(points2, plotter.XY{
+			X: t,
+			Y: logistic(1, 1, 1000, t),
+		})
 	}
-	fmt.Println(logistic(0, 1, 1, 1/100.0))
 	CreateLineplotPlot(points2, "t - growth", l, b, "logistic.png")
 }
